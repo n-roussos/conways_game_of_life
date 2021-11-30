0066f0dc7b40f0eb2bde6ae7f061a1dcb288514a
Nick Roussos <rouni97@hotmail.gr>
2021-11-30 12:10:52 +0100

    Added stud for getter

--- app/src/main/java/base/Grid.java
+++ app/src/main/java/base/Grid.java
@@ -24,13 +24,33 @@ public class Grid {
         int maxX = Integer.MIN_VALUE;
         int maxY = Integer.MIN_VALUE;
 
-//        for(Cell c : aliveCells){
-//
-//            if(c.getPositionX() < minX){
-//                minX =
-//            }
-//        }
+        for(Cell c : aliveCells){
+            int positionX = c.getPositionX();
+            int positionY = c.getPositionY();
+
+            if(positionX < minX){
+                minX = positionX;
+            }
+
+            if(positionY < minY){
+                minY = positionY;
+            }
+
+            if(positionX > maxX){
+                maxX = positionX;
+            }
+
+            if(positionY > maxY){
+                maxY = positionY;
+            }
+        }
+    }
+
+    public List<Point> getAlivePoints(){
+        return new ArrayList<>();
+    }
+
+//    private List<Point> cellsToPoints(){
+//        List<Point> points
 //    }
-//
-//    public
 }

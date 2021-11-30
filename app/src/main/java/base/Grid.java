6888468bf0f7a0edd2b0fc2abfc0ebe81ba4b042
Nick Roussos <rouni97@hotmail.gr>
2021-11-30 12:03:30 +0100

    Replaced constructor with setAlivePoints()

--- app/src/main/java/base/Grid.java
+++ app/src/main/java/base/Grid.java
@@ -1,4 +1,36 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Grid {
+    private List<Cell> aliveCells = new ArrayList<Cell>();
+
+    public void setAlivePoints(List<Point> alivePoints){
+        aliveCells.clear();
+        for(Point p : alivePoints){
+            Cell aliveCell = new Cell(p, true);
+            aliveCells.add(aliveCell);
+        }
+    }
+
+    public void setCellState(Point coordinates, boolean isAlive){
+
+    }
+
+    public void updateToNextIteration(){
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+//        for(Cell c : aliveCells){
+//
+//            if(c.getPositionX() < minX){
+//                minX =
+//            }
+//        }
+//    }
+//
+//    public
 }

5817ad6b897ef39ccbde12fa9af101ad81eab82e
Nick Roussos <rouni97@hotmail.gr>
2021-11-30 11:22:41 +0100

    Added implementation of Cell and Point classes

--- app/src/main/java/base/Cell.java
+++ app/src/main/java/base/Cell.java
@@ -1,4 +1,15 @@
 package base;
 
 public class Cell {
+    private boolean isAlive;
+    private Point coordinates;
+
+    public Cell(Point coordinates, boolean isAlive){
+        this.coordinates = coordinates;
+        this.isAlive = isAlive;
+    }
+
+    public Cell(Point coordinates){
+        this(coordinates, false);
+    }
 }

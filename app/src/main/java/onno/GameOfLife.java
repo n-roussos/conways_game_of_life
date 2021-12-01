4083cfcdc6e7637438b7715f36bfe7a6ae1d4670
Evangelou <g.evangelou@tue.nl>
2021-12-01 13:28:21 +0100

    Added Onno's class + studs

---
+++ app/src/main/java/onno/GameOfLife.java
@@ -0,0 +1,25 @@
+package onno;
+
+import java.awt.*;
+
+public class GameOfLife {
+    public GameOfLife(Object o) {
+
+    }
+
+    public void tick() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+}
